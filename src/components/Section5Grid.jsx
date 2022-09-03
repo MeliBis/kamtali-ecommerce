347d954384b93f0574e93b5cf50c67d16f1b739c
Melissa Barrios <melissa.blecona@gmail.com>
2022-09-03 09:39:59 -0500

    prueba para coleccion de imagenes

--- src/components/Section5Grid.jsx
+++ src/components/Section5Grid.jsx
@@ -9,6 +9,20 @@ import "swiper/css/pagination";
 // import required modules
 import { Grid, Pagination } from "swiper";
 
+const imgGrid1 = require('../assets/section5Grid/mujer1.png')
+const imgGrid2 = require('../assets/section5Grid/mujer2.png')
+const imgGrid3 = require('../assets/section5Grid/mujer3.png')
+const imgGrid4 = require('../assets/section5Grid/mujer4.png')
+const imgGrid5 = require('../assets/section5Grid/mujer5.png')
+const imgGrid6 = require('../assets/section5Grid/mujer6.png')
+const imgGrid7 = require('../assets/section5Grid/hombre1.png')
+const imgGrid8 = require('../assets/section5Grid/hombre2.png')
+const imgGrid9 = require('../assets/section5Grid/hombre3.png')
+
+
+
+
+
 const Section5Grid = () => {
   return (
     <>
@@ -22,17 +36,68 @@ const Section5Grid = () => {
           clickable: true,
         }}
         modules={[Grid, Pagination]}
-        className="mySwiper container"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        className="mySwiper container row row-cols- row-cols-sm-2 row-cols-md-3 g-3 ">
+
+        
+        <SwiperSlide className="r">
+          <div className=" col">
+
+              <img src={imgGrid2} className="bd-placeholder-img card-img-top" alt="..." />
+          </div>
+        </SwiperSlide>
+ 
+        <SwiperSlide className="">
+          <div className=" col">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+              <img src={imgGrid2} className="bd-placeholder-img card-img-top" alt="..." />
+            </div>
+          </div>
+        </SwiperSlide>   <SwiperSlide className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div className=" col">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+              <img src={imgGrid2} className="bd-placeholder-img card-img-top" alt="..." />
+            </div>
+          </div>
+        </SwiperSlide>
+
+   <SwiperSlide className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div className=" col">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+              <img src={imgGrid2} className="bd-placeholder-img card-img-top" alt="..." />
+            </div>
+          </div>
+        </SwiperSlide>
+
+
+
+{/* 
+
+        <SwiperSlide className="row">
+        <img src={imgGrid3} className="col" alt="..." />
+        </SwiperSlide>
+        <SwiperSlide className="row">
+        <img src={imgGrid4} className="col" alt="..." />
+        </SwiperSlide>
+        <SwiperSlide className="row">
+        <img src={imgGrid5} className="col" alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={imgGrid6}  alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={imgGrid7}  alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={imgGrid8}  alt="..." />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src={imgGrid9}  alt="..." />
+
+
+        </SwiperSlide> */}
       </Swiper>
 
     </>
